9061161e7f7e90461c7f4ef24dc70e0abe6c0372
Kostyantyn Simkin <kostyantyn.simkin@posteo.de>
2021-08-17 13:56:58 +0200

    config corrected

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "55t4qdbkpj966b53vrc96e8n8q",     // CognitoClientID
+  "api_base_url": "https://a5iy7avvtj.execute-api.eu-central-1.amazonaws.com/",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-test-refarch.auth.eu-central-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d17vqfe95ohly3.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
