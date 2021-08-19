d264f1845bacf5c92d5f6f56e3f389520911f6b0
Kostyantyn Simkin <kostyantyn.simkin@posteo.de>
2021-08-19 14:52:09 +0200

    update config

--- www/src/config.js
+++ www/src/config.js
@@ -2,9 +2,9 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "1f1frmdo49u6d3c31a3mgt7cep",     // CognitoClientID
+  "aws_user_pools_web_client_id": "237up6g2vbmmb1r31rnevt9if0",     // CognitoClientID
   "api_base_url": "https://5w4exdi737.execute-api.eu-central-1.amazonaws.com/prod",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "mytodoappdemo-test-refarch.auth.eu-central-1.amazoncognito.com",                   // CognitoDomainName
+  "cognito_hosted_domain": "kosimk-test-app.auth.eu-central-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d11gv9881ns0fr.amplifyapp.com"                                      // AmplifyURL
 };
 
